681228523ade3bf957dd5efe378d5503c1c544d3
ade.ristanto@gmail.com <56842597-Ade>
2021-08-17 21:04:18 +0700

    2. Array

--- array.js
+++ array.js
@@ -2,15 +2,46 @@ const nama = ["komang", "made", "ketut"]
 
 console.log(nama)
 
-// console.log("nama ke - " + nama.length)
-
+// Akes array
 // console.log(nama[1])
 
-// Pengulangan
-nama.forEach(function(item, index, array){
-    console.log(item, index, array)
-})
+// Untuk mengetahui jumlah arraynya
+// console.log("Jumlah " + nama.length)
 
-// Penamban di Array
+// Pengulangan
+// nama.forEach(function(item, index, array){
+//     console.log(item, index, array)
+// })
+
+// // Penamban di Array
 nama.push("dewi")
+// nama.push("ketut")
+// console.log(nama)
+
+// Menghapus nilai array dari belakang
+nama.pop()
+// console.log(nama)
+
+// Mehaopus nilai array dari depan
+nama.shift()
+// console.log(nama)
+
+// Menambahkan nilai array dari awal
+nama.unshift("bella")
+// console.log(nama)
+
+// Mencari index didalam suatau item
+// cari = nama.indexOf("ketut") + 1
+// console.log(cari)
+
+// Menhapus item berdasarkan index
+// hapus = nama.indexOf('ketut')
+// console.log(nama.splice(hapus, 1))
+// console.log(nama)
+
+// Menduplikat sebuah Array
+console.log(nama)
+copyNama = nama.slice()
+console.log(copyNama)
+nama.pop()
 console.log(nama)
