2347492c418249f543693f41a2e828fce9593a4f
ade.ristanto@gmail.com <56842597-Ade>
2021-08-17 20:26:20 +0700

    1. Variable

---
+++ var.js
@@ -0,0 +1,35 @@
+// let iceCream = 5;
+
+// console.log(iceCream)
+
+// // iceCream = 12;
+// // console.log(iceCream);
+
+// // const iceCream = 5;
+
+// // console.log(iceCream)
+
+// // iceCream = 12;
+// // console.log(iceCream);
+
+// if (iceCream == 2)
+//     console.log(iceCream);
+
+// else
+//     console.log("Ice Cream tidak sama dengan 2");
+
+// const sayaSukaEsKrim = true;
+// const sayaSukaHumberger = true;
+
+// // Dan
+// // if (sayaSukaEsKrim && sayaSukaHumberger)
+// //     console.log("saya suka eskirm dan humberger");
+
+// // Kebalikan "!"
+// console.log(!sayaSukaHumberger)
+
+const firstname = "Ade"
+const lastname = "Ristanto"
+
+// Penggabungan dengan string literalas `${}` tandanya yg di bawah ESC
+console.log(`${firstname} ${lastname}`)
